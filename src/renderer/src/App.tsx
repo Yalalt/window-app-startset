b8e8c0fa92ab3cc688aae335a9bb19bbc690f6d5
Yalalt Gandush <yalaltgandush@gmail.com>
2024-04-29 15:46:36 +0800

    first:initial files

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,11 @@
+import { RootLayout, SideBar } from './components'
+
+function App() {
+  return (
+    <RootLayout>
+      <SideBar className="p-2 border-4 border-red-500">Sidebar</SideBar>
+    </RootLayout>
+  )
+}
+
+export default App
